af5602726cb25473a74238cb5a576fa8e938a9b0
tonio teran <toniioteran@gmail.com>
2021-01-25 23:59:49 -0800

    add: add unit tests for inverntory functions

--- tests/cv/test_utils.py
+++ tests/cv/test_utils.py
@@ -24,6 +24,30 @@ class TestCvUtils(unittest.TestCase):
           if ds in ds_to_sourcetasks.keys():
             self.assertTrue(source in ds_to_sourcetasks[ds].keys())
 
+  def test_map_dataset_tasks(self):
+    """Make sure the map from dataset to tasks correctly encapsulates info."""
+    ds_to_tasks = utils.map_dataset_tasks()
+
+    for source in utils.DATASET_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.DATASETS:
+        for ds in wrapper.DATASETS[task]:
+          # Account for any spelling discrepancies.
+          if ds in ds_to_tasks.keys():
+            self.assertTrue(task in ds_to_tasks[ds])
+
+  def test_map_dataset_sources(self):
+    """Ensure map from dataset name to available sources is correct."""
+    ds_to_sources = utils.map_dataset_sources()
+
+    for source in utils.DATASET_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.DATASETS:
+        for ds in wrapper.DATASETS[task]:
+          # Account for any spelling discrepancies.
+          if ds in ds_to_sources.keys():
+            self.assertTrue(source in ds_to_sources[ds])
+
 
 if __name__ == "__main__":
   unittest.main()
