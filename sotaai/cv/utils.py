75d5605a3336a5737f5b9ec974fe9bcce1e13547
tonio teran <toniioteran@gmail.com>
2021-01-26 18:36:50 -0800

    Add generalized version of the mappers.

--- sotaai/cv/utils.py
+++ sotaai/cv/utils.py
@@ -2,9 +2,13 @@
 # Author: Tonio Teran <tonio@stateoftheart.ai>
 # Copyright: Stateoftheart AI PBC 2021.
 """Useful utility functions to navigate the library"s available resources."""
+# TODO(tonioteran) Deprecate specific dataset/model functions for the
+# generalized version.
 import importlib
 
-MODEL_SOURCES = ["fastai", "keras", "mxnet", "torch", "pretrainedmodels"]
+# TODO(tonioteran) Currently removed "mxnet" and "pretrainedmodels" from
+# MODEL_SOURCES. Need to restore as soon as the wrapper is done and unit test.
+MODEL_SOURCES = ["fastai", "keras", "torch"]  # "mxnet", "pretrainedmodels"
 
 # TODO(tonioteran) Currently removed "mxnet" from DATASET_SOURCES. Need to
 # restore as soon as the wrapper is done and unit test.
@@ -126,3 +130,55 @@ def map_dataset_info() -> dict:
     }
 
   return dataset_info
+
+
+def map_name_source_tasks(nametype: str) -> dict:
+  """Gathers all models/datasets and their respective sources and tasks.
+
+  Crawls through all modules to arrange entries of the form:
+
+    <dataset-name>: {
+        <name-source-1>: [<supported-task-11>, <supported-task-12>, ...],
+        <name-source-2>: [<supported-task-21>, <supported-task-22>, ...],
+        ...
+        <name-source-n>: [<supported-task-n1>, <supported-task-n2>, ...],
+    }
+
+  Ensures duplicate removals by transforming all strings to lower case, and
+  preserving the original names in an additional `original_names` dictionary.
+
+  Args:
+    nametype (str):
+      Types of names to be used, i.e., either "models" or "datasets".
+
+  Returns (dict):
+    Dictionary with an entry for all available items of the above form.
+
+  TODO(tonioteran) THIS SHOULD BE CACHED EVERY TIME WE USE IT.
+  """
+  items_breakdown = dict()
+  original_names = dict()
+
+  sources = DATASET_SOURCES if nametype == "datasets" else MODEL_SOURCES
+
+  for source in sources:
+    wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+    items = wrapper.DATASETS if nametype == "datasets" else wrapper.MODELS
+    for task in items:
+      for item in items[task]:
+        original_names[item.lower()] = item
+        item = item.lower()
+        if item in items_breakdown.keys():
+          if source in items_breakdown[item].keys():
+            items_breakdown[item][source].append(task)
+          else:
+            items_breakdown[item][source] = [task]
+        else:
+          items_breakdown[item] = {source: [task]}
+  # Uses the entries of `original_names` as keys to store the entries from
+  # the `items_breakdown` dict, which uses lowercase names as keys.
+  output_dict = dict()
+  for itemname in items_breakdown:
+    output_dict[original_names[itemname]] = items_breakdown[itemname]
+
+  return output_dict
