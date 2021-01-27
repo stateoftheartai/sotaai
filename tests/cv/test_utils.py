a8c8aa997ec78c92a819cef8a3ce600a974bb897
tonio teran <toniioteran@gmail.com>
2021-01-26 19:00:25 -0800

    Unit test all generalized mapper functions.

--- tests/cv/test_utils.py
+++ tests/cv/test_utils.py
@@ -85,6 +85,80 @@ class TestCvUtils(unittest.TestCase):
           if model in model_to_sourcetasks.keys():
             self.assertTrue(source in model_to_sourcetasks[model].keys())
 
+  def test_map_name_tasks(self):
+    """Make sure the map from model/dataset to tasks encapsulates info."""
+    # First for datasets.
+    ds_to_tasks = utils.map_name_tasks("datasets")
+
+    for source in utils.DATASET_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.DATASETS:
+        for ds in wrapper.DATASETS[task]:
+          # Account for any spelling discrepancies.
+          if ds in ds_to_tasks.keys():
+            self.assertTrue(task in ds_to_tasks[ds])
+
+    # Then for models.
+    model_to_tasks = utils.map_name_tasks("models")
+
+    for source in utils.MODEL_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.MODELS:
+        for model in wrapper.MODELS[task]:
+          # Account for any spelling discrepancies.
+          if model in model_to_tasks.keys():
+            self.assertTrue(task in model_to_tasks[model])
+
+  def test_map_name_sources(self):
+    """Ensure map from model/dataset name to available sources is correct."""
+    # First for datasets.
+    ds_to_sources = utils.map_name_sources("datasets")
+
+    for source in utils.DATASET_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.DATASETS:
+        for ds in wrapper.DATASETS[task]:
+          # Account for any spelling discrepancies.
+          if ds in ds_to_sources.keys():
+            self.assertTrue(source in ds_to_sources[ds])
+
+    # Then for models.
+    model_to_sources = utils.map_name_sources("models")
+
+    for source in utils.MODEL_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.MODELS:
+        for model in wrapper.MODELS[task]:
+          # Account for any spelling discrepancies.
+          if model in model_to_sources.keys():
+            self.assertTrue(source in model_to_sources[model])
+
+  def test_map_name_info(self):
+    """Ensure tasks and sources are adequately parsed."""
+    # First for datasets.
+    ds_to_info = utils.map_name_info("datasets")
+
+    for source in utils.DATASET_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.DATASETS:
+        for ds in wrapper.DATASETS[task]:
+          # Account for any spelling discrepancies.
+          if ds in ds_to_info.keys():
+            self.assertTrue(task in ds_to_info[ds]["tasks"])
+            self.assertTrue(source in ds_to_info[ds]["sources"])
+
+    # Then for models.
+    models_to_info = utils.map_name_info("models")
+
+    for source in utils.MODEL_SOURCES:
+      wrapper = importlib.import_module("sotaai.cv." + source + "_wrapper")
+      for task in wrapper.MODELS:
+        for model in wrapper.MODELS[task]:
+          # Account for any spelling discrepancies.
+          if model in models_to_info.keys():
+            self.assertTrue(task in models_to_info[model]["tasks"])
+            self.assertTrue(source in models_to_info[model]["sources"])
+
 
 if __name__ == "__main__":
   unittest.main()
