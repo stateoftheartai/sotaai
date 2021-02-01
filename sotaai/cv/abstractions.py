bf7e35b045807db1fd3283d4050f6ea3b3e06366
tonio teran <toniioteran@gmail.com>
2021-02-01 00:09:20 -0800

    update: finish model scaffolding.

--- sotaai/cv/abstractions.py
+++ sotaai/cv/abstractions.py
@@ -31,24 +31,25 @@ class CvModel(object):
   Each abstract `CvModel` represents a model from one of the sources.
   """
 
-  def __init__(self, raw_object, name: str):
-    """Constructor using `raw_object` from a source library.
+  def __init__(self, raw_model, name: str):
+    """Constructor using `raw_model` from a source library.
 
     Args:
-      raw_object:
+      raw_model:
         Model object directly instantiated from a source library. Type
         is dependent on the source library.
       name (str):
         Name of the model.
     """
-    self.raw = raw_object
+    self.raw = raw_model
     self.name = name
-    self.source = utils.extract_source_from_model(raw_object)
-    self.input_type = None  # TODO(tonioteran) Implement me.
+    self.source = utils.get_source_from_model(raw_model)
+    self.original_input_type = utils.get_input_type(raw_model)
+    self.data_type = None  # TODO(tonioteran) Implement me.
     self.min_size = None  # TODO(tonioteran) Implement me.
-    self.num_channels = None  # TODO(tonioteran) Implement me.
-    self.num_layers = None  # TODO(tonioteran) Implement me.
-    self.num_params = None  # TODO(tonioteran) Implement me.
+    self.num_channels = utils.get_num_channels_from_model(raw_model)
+    self.num_layers = utils.get_num_layers_from_model(raw_model)
+    self.num_params = utils.get_num_parameters_from_model(raw_model)
     self.associated_datasets = None  # TODO(tonioteran) Implement me.
     self.paper = None  # TODO(tonioteran) Implement me.
 
@@ -60,4 +61,12 @@ class CvModel(object):
     that we have the correct number of outputs in the last layer to use the
     dataset, etc.
     """
-    raise NotImplementedError
+    # This, I believe, should more or less be the workflow:
+    #
+    # 1. Transform the `input_data` into something that `self.raw` can
+    # understand.
+    # 2. The `self.raw` object should already be able to process the
+    # transformed `input_data`, so just directly feed it as:
+    #
+    #  return self.raw(transformed_input_data)
+    raise NotImplementedError("Still need to implement the evaluation fxn.")
