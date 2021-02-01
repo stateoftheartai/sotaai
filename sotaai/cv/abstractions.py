af30fb8a6cb0a4379cf8db05324657d3c990fb16
tonio teran <toniioteran@gmail.com>
2021-01-31 23:39:45 -0800

    update: template attributes for CvModel class.

--- sotaai/cv/abstractions.py
+++ sotaai/cv/abstractions.py
@@ -2,6 +2,7 @@
 # Author: Tonio Teran
 # Copyright: Stateoftheart AI PBC 2020.
 """Abstract classes for standardized models and datasets."""
+from sotaai.cv import utils
 
 
 class CvDataset(object):
@@ -42,3 +43,21 @@ class CvModel(object):
     """
     self.raw = raw_object
     self.name = name
+    self.source = utils.extract_source_from_model(raw_object)
+    self.input_type = None  # TODO(tonioteran) Implement me.
+    self.min_size = None  # TODO(tonioteran) Implement me.
+    self.num_channels = None  # TODO(tonioteran) Implement me.
+    self.num_layers = None  # TODO(tonioteran) Implement me.
+    self.num_params = None  # TODO(tonioteran) Implement me.
+    self.associated_datasets = None  # TODO(tonioteran) Implement me.
+    self.paper = None  # TODO(tonioteran) Implement me.
+
+  def __call__(self, input_data):
+    """TODO(tonioteran) Define `input_data` type for standardization.
+
+    Here, compatibility between the `input_data` type and the model must have
+    been already ensured, e.g., that the image/video is of the correct size,
+    that we have the correct number of outputs in the last layer to use the
+    dataset, etc.
+    """
+    raise NotImplementedError
