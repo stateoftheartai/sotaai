75b619e2489731f1548fc805f1399aaeabf85545
Antonio Terán <teran@mit.edu>
2021-02-20 15:05:53 -0800

    add: basic infra for RL, plus gym and mxnet to pip requirements
    * update: create infra for RL directories
    * update: add test directory for RL
    * update: gym wrapper skeleton
    * update: add gym to install requirements
    * add: placeholder doc for rl garage
    * add: mxnet to setup.py (why wasn't it there before?)
    - SOTA-1616 #done

---
+++ sotaai/rl/gym_wrapper.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Author: Tonio Teran <tonio@stateoftheart.ai>
+# Copyright: Stateoftheart AI PBC 2021.
+'''OpenAI's Gym library wrapper.'''
+import gym  # pylint: disable=unused-import
