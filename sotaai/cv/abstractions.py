0322390cc10e3d9971b5281eb7e4b15c2be780fb
Hugo Ochoa <hugo.ochoa.h@gmail.com>
2021-02-22 07:06:26 -0600

    fix: fix utils test for Keras and Tensorflow
    - Fix get_source_name in utils for Keras and TF
    - Fix get_num_classes in utils for Keras and TF
    - Add wider_face to TF tests (object detection)
    - Rename object detection task to object_detection
    - Add utils.compare_shapes to match shapes in tests
    - SOTA-1615

--- sotaai/cv/abstractions.py
+++ sotaai/cv/abstractions.py
@@ -26,7 +26,7 @@ class CvDataset(object):
     self.raw = raw_dataset
     self.iterator = iterator
     self.name = name
-    self.source = utils.get_source_from_dataset(raw_dataset[split_name])
+    self.source = utils.get_source_from_dataset(raw_dataset)
     self.data_type = None  # TODO(tonioteran) Implement me.
     self.split_name = split_name
     self.tasks = utils.map_dataset_tasks()[name]
@@ -39,16 +39,14 @@ class CvDataset(object):
     # Populated for datasets supporting classification or detection tasks.
     self.classes = None
     self.classes_names = None
-    if 'classification' in self.tasks or 'object detection' in self.tasks:
+    if 'classification' in self.tasks or 'object_detection' in self.tasks:
       self.classes, self.classes_names = utils.get_classes_from_dataset(
           raw_dataset, self.source, self.name, self.split_name, self.size)
 
     # Only populated for datasets that support segmentation tasks.
     self.pixel_types = None
     self.pixel_types_names = None
-    print(self.tasks)
     if 'segmentation' in self.tasks:
-      print('entra aqui')
       self.pixel_types, self.pixel_types_names = (utils.extract_pixel_types(
           raw_dataset, self.name, self.source, self.split_name))
       # self.pixel_types, self.pixel_types_names = (
