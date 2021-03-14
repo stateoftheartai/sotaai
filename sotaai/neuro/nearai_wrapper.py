8c03b5e8069205ce17b7c6dfd7114e3a5e189b58
tonio teran <toniioteran@gmail.com>
2021-03-14 00:51:38 -0800

    add: info for near ai

---
+++ sotaai/neuro/nearai_wrapper.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Author: Tonio Teran <tonio@stateoftheart.ai>
+# Copyright: Stateoftheart AI PBC 2021.
+'''NEAR AI's library wrapper.
+
+Dataset information taken from:
+'''
+
+DATASETS = {'program synthesis': ['AlgoLisp', 'Karel', 'NAPS']}
