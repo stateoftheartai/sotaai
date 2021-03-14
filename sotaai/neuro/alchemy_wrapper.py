4349124b985f210448e3f52c263dd64fc62b0c23
tonio teran <toniioteran@gmail.com>
2021-03-14 00:16:34 -0800

    add: alchemy information

---
+++ sotaai/neuro/alchemy_wrapper.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Author: Tonio Teran <tonio@stateoftheart.ai>
+# Copyright: Stateoftheart AI PBC 2021.
+'''Alchemy's library wrapper.
+
+Dataset information taken from: https://alchemy.cs.washington.edu/data/
+'''
+
+DATASETS = {
+    'unknown': [
+        'Animals', 'Citeseer', 'Cora', 'Epinions', 'IMDB', 'Kinships',
+        'Nations', 'ProteinInteraction', 'RadishRobotMapping', 'Tutorial',
+        'UMLS', 'UW-CSE', 'WebKB'
+    ]
+}
