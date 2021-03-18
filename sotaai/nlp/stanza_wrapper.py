24e9eea1a4c17f9842cb1697fd53512e9aa95a08
Antonio Terán <teran@mit.edu>
2021-03-17 22:34:38 -0700

    add: model and dataset information for dataset libraries
    * add: allennlp models and datasets
    
    * add: decanlp models and tasks
    
    * add: fairseq models and datasets
    
    * add: flair datasets and models
    
    * fix: linting fairseq
    
    * add: hanlp models and datasets.
    
    * add:nlp architect info
    
    * add: parlai info
    
    * add: stanza info
    
    * add: TFDS info
    
    * add: torchtext datasets

---
+++ sotaai/nlp/stanza_wrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Author: Tonio Teran
+# Copyright: Stateoftheart AI PBC 2021.
+'''Stanza's wrapper module.
+
+Model information taken from:
+https://github.com/stanfordnlp/stanza/tree/main/stanza/models
+Dataset information taken from:
+'''
+
+MODELS = {
+    'text classification': ['CNNClassifier'],
+    'dependency parsing': ['Parser'],
+    'named entity recongnition': ['NERTagger'],
+    'part-of-speech tagging': ['Tagger'],
+    'tokenization': ['Tokenizer'],
+    'clinical syntactic analysis': ['craft', 'genia', 'mimic'],
+    'clinical named entity recognition': [
+        'anatem', 'bc5cdr', 'bc4chemd', 'bionlp13cg', 'jnlpba', 'linnaeus',
+        'ncbi_disease', 's800', 'i2b2', 'radiology'
+    ]
+}
