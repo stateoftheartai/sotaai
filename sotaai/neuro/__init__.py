d9583cafecc2b82005f0d42faad989feb827974d
tonio teran <toniioteran@gmail.com>
2021-03-18 22:21:47 -0700

    add: load models and datasets module level neuro

--- sotaai/neuro/__init__.py
+++ sotaai/neuro/__init__.py
@@ -5,3 +5,82 @@
 from sotaai.neuro import utils
 from sotaai.neuro import abstractions
 import importlib
+
+
+def load_model(name: str) -> abstractions.NeuroModel:
+  '''Dummy load model function. Placeholder for real wrapper.'''
+  model_source_map = utils.map_name_sources('models')
+  source = model_source_map[name][0]
+  wrapper = importlib.import_module('sotaai.neuro.' + source + '_wrapper')
+  raw_object = wrapper.load_model(name)
+  return abstractions.NeuroModel(raw_object['name'], raw_object['source'])
+
+
+def load_dataset(name: str) -> abstractions.NeuroDataset:
+  '''Dummy load dataset function. Placeholder for real wrapper.'''
+  dataset_source_map = utils.map_name_sources('datasets')
+  source = dataset_source_map[name][0]
+  wrapper = importlib.import_module('sotaai.neuro.' + source + '_wrapper')
+  raw_object = wrapper.load_dataset(name)
+  return abstractions.NeuroDataset(raw_object['name'], raw_object['source'])
+
+
+def create_models_dict(model_names, models_sources_map):
+  '''Given a list of model names, return a list with the JSON representation
+  of each model as an standardized dict
+  Args:
+    model_names (list): list of model names to return the standardized dict
+    models_sources_map: a dict map between model names and sources as returned
+      by the utils function map_name_sources('models')
+  Returns:
+    A list of dictionaries with the JSON representation of each CV model
+  '''
+
+  print('\nCreating model JSONs...')
+
+  models = []
+
+  for i, model_name in enumerate(model_names):
+    print(' - ({}/{}) {}'.format(i + 1, len(model_names),
+                                 'models.' + model_name))
+    model = load_model(model_name)
+    model_dict = model.to_dict()
+
+    model_dict['sources'] = models_sources_map[model_dict['name']]
+    del model_dict['source']
+
+    models.append(model_dict)
+
+  return models
+
+
+def create_datasets_dict(dataset_names, dataset_sources_map):
+  '''Given a list of dataset names, return a list with the JSON representation
+  of each dataset as an standardized dict
+
+  Args:
+    dataset_names (list): list of dataset names to return the standardized dict
+      dataset
+    dataset_sources_map: a dict map between dataset names and sources as
+      returned by the utils function map_name_sources('datasets')
+
+  Returns:
+    A list of dictionaries with the JSON representation of each CV model
+  '''
+
+  print('\nCreating model JSONs...')
+
+  datasets = []
+
+  for i, dataset_name in enumerate(dataset_names):
+    print(' - ({}/{}) {}'.format(i + 1, len(dataset_names),
+                                 'datasets.' + dataset_name))
+    dataset = load_dataset(dataset_name)
+    dataset_dict = dataset.to_dict()
+
+    dataset_dict['sources'] = dataset_sources_map[dataset_dict['name']]
+    del dataset_dict['source']
+
+    dataset.append(dataset_dict)
+
+  return datasets
