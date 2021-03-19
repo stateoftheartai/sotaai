f4411748dc779b2c0c8b8e43b34613c0448d2806
tonio teran <toniioteran@gmail.com>
2021-03-18 21:43:55 -0700

    add: modules to neuro init

--- sotaai/neuro/__init__.py
+++ sotaai/neuro/__init__.py
@@ -2,3 +2,6 @@
 # Author: Tonio Teran <tonio@stateoftheart.ai>
 # Copyright: Stateoftheart AI PBC 2021.
 '''Main neurosymbolic-reasoning module to standardize.'''
+from sotaai.neuro import utils
+from sotaai.neuro import abstractions
+import importlib
