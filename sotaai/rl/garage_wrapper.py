f52ae5a93681130a1ca8b65aff8fb6d12af97f45
tonio teran <toniioteran@gmail.com>
2021-03-23 00:45:15 -0700

    add: loaders and metadata for garage

---
+++ sotaai/rl/garage_wrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Author: Tonio Teran <tonio@stateoftheart.ai>
+# Copyright: Stateoftheart AI PBC 2021.
+'''Garage's library wrapper.'''
+
+SOURCE_METADATA = {
+    'name': 'garage',
+    'original_name': '',
+    'url': 'https://garage.readthedocs.io/en/latest/index.html'
+}
+
+MODELS = {
+    'unknown': [
+        'CEM', 'CMA-ES', 'REINFORCE', 'VPG', 'DDPG', 'DQN', 'ERWR', 'NPO',
+        'PPO', 'REPS', 'TD3', 'TNPG', 'TRPO', 'MAML', 'RL2', 'PEARL', 'SAC',
+        'MTSAC', 'MTPPO', 'MTTRPO', 'TaskEmbedding', 'BehavioralCloning'
+    ],
+}
+
+
+def load_model(name: str) -> dict:
+  return {'name': name, 'source': 'garage'}
