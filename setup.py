a448dbc4199c17c12e7a441fc621a80d2c989f4e
Hugo Ochoa <hugo.ochoa.h@gmail.com>
2021-03-25 20:36:47 -0600

    update: update python dependencies and setup
    - Update setup to install dependencies by area
    - Update docs
    - Add examples dir
    SOTA-1654

--- setup.py
+++ setup.py
@@ -5,15 +5,20 @@ Core sotaai library setup file
 from setuptools import setup
 
 setup(name='sotaai',
-      version='0.0.4',
+      version='0.0.5',
       author='Stateoftheart AI PBC',
       author_email='admin@stateoftheart.ai',
       description='Stateoftheart AI Official Library',
       url='https://github.com/stateoftheartai/sotaai',
-      packages=['sotaai', 'sotaai.cv'],
-      install_requires=[
-          'tensorflow', 'tensorflow-datasets', 'gym', 'torchvision', 'torch',
-          'transformers', 'datasets', 'scikit-image', 'opencv-python==4.5.1.48'
-      ],
+      packages=['sotaai'],
+      install_requires=[],
       python_requires='>=3,<3.9',
-      extras_require={})
+      extras_require={
+          'cv': [
+              'tensorflow', 'tensorflow-datasets', 'torchvision', 'torch',
+              'transformers', 'scikit-image', 'opencv-python==4.5.1.48'
+          ],
+          'neuro': [],
+          'nlp': ['datasets'],
+          'rl': ['gym']
+      })
