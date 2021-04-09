11c065b29b230f099e9d6d984190c7c162598c65
Eduardo Espinosa <edan@Eduardos-MacBook-Pro.local>
2021-04-09 11:44:55 -0500

    fix: Setup was broken - Wrong way to add git urls at extra_requires

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='sotaai',
           'neuro': [],
           'nlp': ['datasets'],
           'rl': [
-              'gym', 'procgen', 'gym-minigrid', 'box2d', 'gym[atari]'
-              'git+https://github.com/benelot/pybullet-gym'
+              'gym', 'procgen', 'gym-minigrid', 'box2d', 'gym[atari]',
+              'pybullet-gym @ git+https://github.com/benelot/pybullet-gym'
           ]
       })
