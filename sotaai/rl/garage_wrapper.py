f358c6144ccdecc688c9bf8845a2e9c13f2d13cf
Jorge Delgadillo <jorgdelgadillo@gmail.com>
2021-04-20 11:39:53 -0500

    refactor: refactor garage models initializations

--- sotaai/rl/garage_wrapper.py
+++ sotaai/rl/garage_wrapper.py
@@ -2,14 +2,19 @@
 # Author: Tonio Teran <tonio@stateoftheart.ai>
 # Copyright: Stateoftheart AI PBC 2021.
 '''Garage's library wrapper.'''
+import torch
 from torch.nn import functional as F
 
+import tensorflow as tf
 from garage.experiment import MetaEvaluator  # For MAMLVPG.
 from garage.experiment.task_sampler import SetTaskSampler  # For MAMLVPG.
-from garage.envs import GarageEnv, normalize
+from garage.envs import normalize, GymEnv
+from garage import Wrapper
+from garage.sampler import LocalSampler
 from garage.torch import algos
-from garage.torch.algos import BC, DDPG, MAMLPPO, MAMLTRPO, MAMLVPG, MTSAC, PEARL, SAC
-from garage.torch.policies import DeterministicMLPPolicy, TanhGaussianMLPPolicy
+from garage.torch.algos import BC, DDPG, MAMLPPO, MAMLTRPO, MAMLVPG, MTSAC, PEARL, SAC, TRPO, VPG
+from garage.torch.policies import DeterministicMLPPolicy, TanhGaussianMLPPolicy, GaussianMLPPolicy
+from garage.tf.policies import CategoricalMLPPolicy
 from garage.torch.value_functions import GaussianMLPValueFunction
 from garage.torch.q_functions import ContinuousMLPQFunction  # For DDPG.
 from garage.replay_buffer import PathBuffer  # For DDPG.
@@ -28,12 +33,20 @@ MODELS = {
     ],
 }
 
+hyper_parameters = {
+    'hidden_sizes': [32, 32],
+    'max_kl': 0.01,
+    'gae_lambda': 0.97,
+    'discount': 0.99,
+    'n_epochs': 999,
+    'batch_size': 1024,
+}
+
 
 def load_model(
     name: str,
     gym_env=None,
     name_env='CartPole-v1',
-    max_path_length=100,
     meta_batch_size=20,
     discount=0.99,
     gae_lambda=1.,
@@ -51,10 +64,12 @@ def load_model(
               estimation.
       inner_lr (float): Adaptation learning rate.
   '''
-  if gym_env is not None:
-    env = GarageEnv(env=gym_env)
-  else:
-    env = GarageEnv(env_name=name_env)
+  gym_env = normalize(GymEnv(name_env))
+  env = Wrapper(gym_env)
+  # if gym_env is not None:
+  #   env = GarageEnv(env=gym_env)
+  # else:
+  #   env = GarageEnv(env_name=name_env)
 
   policy = DeterministicMLPPolicy(name='policy',
                                   env_spec=env.spec,
@@ -65,19 +80,24 @@ def load_model(
     vfunc = GaussianMLPValueFunction(env_spec=env.spec)
 
     task_sampler = SetTaskSampler(
-        lambda: GarageEnv(normalize(env, expected_action_scale=10.)))
+        lambda: normalize(GymEnv(name_env), expected_action_scale=10.))
 
     meta_evaluator = MetaEvaluator(test_task_sampler=task_sampler,
-                                   max_path_length=max_path_length,
-                                   n_test_tasks=1,
-                                   n_test_rollouts=10)
+                                   n_test_tasks=1)
+
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
     algo = MAMLTRPO(env=env,
                     policy=policy,
                     value_function=vfunc,
-                    max_path_length=max_path_length,
                     meta_batch_size=meta_batch_size,
                     discount=discount,
                     gae_lambda=gae_lambda,
+                    task_sampler=task_sampler,
+                    sampler=sampler,
                     inner_lr=inner_lr,
                     num_grad_updates=1,
                     meta_evaluator=meta_evaluator)
@@ -86,19 +106,18 @@ def load_model(
     vfunc = GaussianMLPValueFunction(env_spec=env.spec)
 
     task_sampler = SetTaskSampler(
-        lambda: GarageEnv(normalize(env, expected_action_scale=10.)))
+        lambda: normalize(GymEnv(name_env), expected_action_scale=10.))
 
     meta_evaluator = MetaEvaluator(test_task_sampler=task_sampler,
-                                   max_path_length=max_path_length,
-                                   n_test_tasks=1,
-                                   n_test_rollouts=10)
+                                   n_test_tasks=1)
     algo = MAMLPPO(env=env,
                    policy=policy,
                    value_function=vfunc,
-                   max_path_length=max_path_length,
                    meta_batch_size=meta_batch_size,
                    discount=discount,
                    gae_lambda=gae_lambda,
+                   task_sampler=task_sampler,
+                   sampler=sampler,
                    inner_lr=inner_lr,
                    num_grad_updates=1,
                    meta_evaluator=meta_evaluator)
@@ -107,19 +126,23 @@ def load_model(
     vfunc = GaussianMLPValueFunction(env_spec=env.spec)
 
     task_sampler = SetTaskSampler(
-        lambda: GarageEnv(normalize(env, expected_action_scale=10.)))
+        lambda: normalize(GymEnv(name_env), expected_action_scale=10.))
 
     meta_evaluator = MetaEvaluator(test_task_sampler=task_sampler,
-                                   max_path_length=max_path_length,
-                                   n_test_tasks=1,
-                                   n_test_rollouts=10)
+                                   n_test_tasks=1)
+
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
     algo = MAMLVPG(env=env,
                    policy=policy,
                    value_function=vfunc,
-                   max_path_length=max_path_length,
                    meta_batch_size=meta_batch_size,
                    discount=discount,
                    gae_lambda=gae_lambda,
+                   task_sampler=task_sampler,
+                   sampler=sampler,
                    inner_lr=inner_lr,
                    num_grad_updates=1,
                    meta_evaluator=meta_evaluator)
@@ -133,13 +156,18 @@ def load_model(
                                  hidden_sizes=[64, 64],
                                  hidden_nonlinearity=F.relu)
 
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
     replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
     algo = SAC(env_spec=env.spec,
                policy=policy,
                qf1=qf1,
                qf2=qf2,
+               sampler=sampler,
                gradient_steps_per_itr=1000,
-               max_path_length=500,
                replay_buffer=replay_buffer)
     return algo
   elif name == 'DDPG':
@@ -147,13 +175,65 @@ def load_model(
                                 hidden_sizes=[64, 64],
                                 hidden_nonlinearity=F.relu)
     replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
+
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
     algo = DDPG(env_spec=env.spec,
                 policy=policy,
-                max_path_length=250,
+                sampler=sampler,
                 qf=qf,
                 replay_buffer=replay_buffer)
     return algo
-  elif name in ('VPG', 'TRPO', 'PPO'):
+  if name == 'TRPO':
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
+    policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=hyper_parameters['hidden_sizes'],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(32, 32),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+
+    algo = TRPO(env_spec=env.spec,
+                policy=policy,
+                sampler=sampler,
+                value_function=value_function)
+
+    return algo
+
+  if name == 'VPG':
+
+    tf.compat.v1.enable_eager_execution()
+
+    policy = CategoricalMLPPolicy(name='policy',
+                                  env_spec=env.spec,
+                                  hidden_sizes=(32, 32))
+
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
+    vfunc = GaussianMLPValueFunction(env_spec=env.spec)
+
+    algo = VPG(env_spec=env.spec,
+               policy=policy,
+               value_function=vfunc,
+               sampler=sampler,
+               discount=0.99)
+
+    return algo
+
+  elif name == 'PPO':
     vfunc = GaussianMLPValueFunction(env_spec=env.spec)
     algo = getattr(algos, name)(env_spec=env.spec,
                                 policy=policy,
@@ -161,11 +241,7 @@ def load_model(
     return algo
   elif name == 'BC':
 
-    algo = BC(env_spec=env.spec,
-              batch_size=20,
-              learner=policy,
-              source=policy,
-              max_path_length=max_path_length)
+    algo = BC(env_spec=env.spec, batch_size=20, learner=policy, source=policy)
   elif name == 'MTSAC':
 
     qf1 = ContinuousMLPQFunction(env_spec=env.spec,
@@ -175,13 +251,18 @@ def load_model(
                                  hidden_sizes=[64, 64],
                                  hidden_nonlinearity=F.relu)
 
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
     replay_buffer = PathBuffer(capacity_in_transitions=int(1e6))
     algo = MTSAC(env_spec=env.spec,
                  policy=policy,
                  qf1=qf1,
                  qf2=qf2,
                  num_tasks=100,
-                 max_path_length=max_path_length,
+                 sampler=sampler,
                  eval_env=env,
                  gradient_steps_per_itr=200,
                  replay_buffer=replay_buffer)
@@ -195,10 +276,10 @@ def load_model(
                             encoder_hidden_size)
 
     # Create multi-task environment and sample tasks.
-    env_start = GarageEnv(env_name=name_env)
-    env_sampler = SetTaskSampler(lambda: GarageEnv(normalize(env_start)))
+    env_start = GymEnv(name_env)
+    env_sampler = SetTaskSampler(lambda: normalize(GymEnv(name_env)))
     env = env_sampler.sample(num_train_tasks)
-    test_env_sampler = SetTaskSampler(lambda: GarageEnv(normalize(env_start)))
+    test_env_sampler = SetTaskSampler(lambda: normalize(GymEnv(env_start)))
 
     # Instantiate networks.
     augmented_env = PEARL.augment_env_spec(env[0](), latent_size)
@@ -212,10 +293,16 @@ def load_model(
     inner_policy = TanhGaussianMLPPolicy(
         env_spec=augmented_env, hidden_sizes=[net_size, net_size, net_size])
 
+    sampler = LocalSampler(agents=policy,
+                           envs=env,
+                           max_episode_length=env.spec.max_episode_length,
+                           is_tf_worker=True)
+
     pearl = PEARL(env=env,
                   inner_policy=inner_policy,
                   qf=qf,
                   vf=vf,
+                  sampler=sampler,
                   num_train_tasks=num_train_tasks,
                   num_test_tasks=num_test_tasks,
                   latent_dim=latent_size,
--- tests/rl/test_garage_wrapper.py
+++ tests/rl/test_garage_wrapper.py
@@ -7,7 +7,10 @@
 import unittest
 from sotaai.rl import gym_wrapper, garage_wrapper, load_model
 from sotaai.rl.abstractions import RlModel
-
+# from garage.trainer import Trainer
+from garage.experiment import SnapshotConfig
+from garage.trainer import Trainer
+from garage.envs import GymEnv, normalize
 env_error = [
     'Defender-v0', 'Defender-v4', 'DefenderDeterministic-v0',
     'DefenderDeterministic-v4', 'DefenderNoFrameskip-v0',
@@ -27,18 +30,49 @@ EROR_MODELS = ['DDPG', 'PEARL']
 class TestGarageWrapper(unittest.TestCase):
   '''Test the wrapped garage module.'''
 
-  @unittest.SkipTest
   def test_load_model(self):
     '''
       Make sure garage envs are returned.
     '''
-    for model_name in MODELS:
-      if not model_name in EROR_MODELS:
-        for task in gym_wrapper.LIST_ENVIRONMENTS:
-          for env_name in gym_wrapper.LIST_ENVIRONMENTS[task]:
-            print(f'Model name: {model_name}\t---\t Env name: {env_name}')
-            garage_wrapper.load_model(name=model_name, name_env=env_name)
 
+    hyper_parameters = {
+        'hidden_sizes': [32, 32],
+        'max_kl': 0.01,
+        'gae_lambda': 0.97,
+        'discount': 0.99,
+        'n_epochs': 999,
+        'batch_size': 1024,
+    }
+
+    # env_gym = gym_wrapper.load_environment('CartPole-v0')
+    env = normalize(GymEnv('CartPole-v0'))
+    algo = garage_wrapper.load_model(name='TRPO', name_env='CartPole-v0')
+
+    ctxt = SnapshotConfig(snapshot_dir='~/experiments',
+                          snapshot_mode='all',
+                          snapshot_gap=1)
+    # deterministic.set_seed(10)
+    trainer = Trainer(ctxt)
+
+    trainer.setup(algo, env)
+    trainer.train(n_epochs=hyper_parameters['n_epochs'],
+                  batch_size=hyper_parameters['batch_size'])
+
+    env.close()
+    # trainer.train(n_epochs=hyper_parameters['n_epochs'],
+    #               batch_size=hyper_parameters['batch_size'])
+
+    # with TFTrainer(ctxt) as trainer:
+    #   trainer.setup(algo, env)
+    #   trainer.train(n_epochs=100, batch_size=4000)
+    # for model_name in MODELS:
+    #   if not model_name in EROR_MODELS:
+    #     for task in gym_wrapper.LIST_ENVIRONMENTS:
+    #       for env_name in gym_wrapper.LIST_ENVIRONMENTS[task]:
+    #         print(f'Model name: {model_name}\t---\t Env name: {env_name}')
+    #         garage_wrapper.load_model(name=model_name, name_env=env_name)
+
+  @unittest.SkipTest
   def test_abstraction_load_model(self):
     '''
       Make sure garage envs are returned.
