269e5c450e3d5abd2b985fb8284a7602165cf94e
Hugo Ochoa <hugo.ochoa.sota@gmail.com>
2021-05-10 07:16:44 -0500

    update: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Core sotaai library setup file
 from setuptools import setup
 
 setup(name='sotaai',
-      version='0.0.7',
+      version='0.0.8',
       author='Stateoftheart AI PBC',
       author_email='admin@stateoftheart.ai',
       description='Stateoftheart AI Official Library',
